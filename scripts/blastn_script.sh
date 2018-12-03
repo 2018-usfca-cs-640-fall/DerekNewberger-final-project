0e161028cc65c278cfb86af59f0ccf31bc08deba
Derek Newberger <drnewberger@dons.usfca.edu>
2018-12-03 14:50:04 -0800

    Pass Travis and add .bib file
    The intoduction is being written and so a .bib file
    was modified. The .sh script did not pass travis,
    a "" has been added to SRA_number.

--- scripts/blastn_script.sh
+++ scripts/blastn_script.sh
@@ -4,5 +4,5 @@ export PATH=$PATH:/Users/derek_newberger/sratoolkit.2.9.2-mac64/bin
 
 for SRA_number in $(cut -f 7 data/metadata/SraRunTable.txt | tail -n +2)
 do
-   fastq-dump -v $SRA_number -O data/raw_data
+   fastq-dump -v "$SRA_number" -O data/raw_data
 done
